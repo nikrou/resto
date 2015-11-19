6a0056eb2662d3b859648883e370c05b7a7c38c7
Nicolas <nikrou77@gmail.com>
2015-11-19 16:03:00 +0100

    thead for users table like in DC

--- client/app-users/src/components/Users.js
+++ client/app-users/src/components/Users.js
@@ -4,17 +4,27 @@ import User from './User';
 class Users extends React.Component
 {
     render() {
-		const users = this.props.names.map(function(name) {
-		    return (<User user={name} />)
-		});
-		return (
-		    <table>
+	const users = this.props.names.map(function(name) {
+	    return (<User user={name} />)
+	});
+
+	return (
+	    <div className="table-outer clear">
+		<table>
 		    <caption className="hidden">Liste des utilisateurs</caption>
 		    <tbody>
-		    {users}
+			<tr>
+			    <th colSpan="2" scope="col" className="first">Identifiant</th>
+			    <th scope="col">Prénom</th>
+			    <th scope="col">Nom</th>
+			    <th scope="col">Pseudonyme</th>
+			    <th scope="col" className="nowrap">Entrées</th>
+			</tr>
+			{users}
 		    </tbody>
-		    </table>
-		);
+		</table>
+	    </div>
+	);
     }
 }
 
