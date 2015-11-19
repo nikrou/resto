3e8c555f1b6fcdd7a82c71f500d585437a6ec9c5
franck <carnet.franck.paul@gmail.com>
2015-11-19 15:46:15 +0100

    no message

--- client/app-users/src/components/Hello.js
+++ client/app-users/src/components/Hello.js
@@ -3,14 +3,14 @@ import React from 'react';
 class Hello extends React.Component
 {
     render() {
-	const hellos = this.props.names.map(function(name) {
-	    return (<h4>Hello {name}!</h4>)
-	});
-	return (
-	    <div>
-	    {hellos}
-	    </div>
-	);
+		const hellos = this.props.names.map(function(name) {
+		    return (<h4>Hello {name.user_id}!</h4>)
+		});
+		return (
+		    <div>
+		    {hellos}
+		    </div>
+		);
     }
 }
 
--- client/app-users/src/containers/App.js
+++ client/app-users/src/containers/App.js
@@ -5,7 +5,51 @@ import Hello from '../components/Hello';
 class App extends Component {
 	render() {
 	    return (
-		<Hello names={['World', 'Master']} />
+		<Hello names={
+			[
+			    {
+			    'user_id':'admin',
+			    'user_superadmin':false,
+			    'user_name':null,
+			    'user_firstname':null,
+			    'user_displayname':null,
+			    'entries':168
+			    },
+			    {
+			    'user_id':'bidule',
+			    'user_superadmin':false,
+			    'user_name':'Chouette',
+			    'user_firstname':'Bidule',
+			    'user_displayname':'bidibulle',
+			    'entries':0,
+			    'admin':true
+			    },
+			    {
+			    'user_id':'nicolas',
+			    'user_superadmin':true,
+			    'user_name':'',
+			    'user_firstname':'',
+			    'user_displayname':'',
+			    'entries':134
+			    },
+			    {
+			    'user_id':'titi',
+			    'user_superadmin':false,
+			    'user_name':'Titi',
+			    'user_firstname':'',
+			    'user_displayname':'',
+			    'entries':0
+			    },
+			    {
+			    'user_id':'joe',
+			    'user_superadmin':false,
+			    'user_name':'Joe',
+			    'user_firstname':'',
+			    'user_displayname':'',
+			    'entries':10
+			    }
+			]
+		} />
 	    );
 	}
 }
