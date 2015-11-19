728612907ed0376d6c53b16ef7f03e42408892a2
Nicolas <nikrou77@gmail.com>
2015-11-19 15:43:40 +0100

    User component for users table

---
+++ client/app-users/src/components/User.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class User extends Component
+{
+    render() {
+	return (
+	    return (
+		<tr className="line">
+		    <td className="nowrap"><input type="checkbox" name="users[]" value="{this.props.user.user_id}" /></td>
+		    <td className="maximal" scope="row"><a href={'user.php?id='+this.props.user.user_id}>{this.props.user.user_id}</a>&nbsp;</td>
+		    <td className="nowrap">{this.props.user.user_firstname}</td>
+		    <td className="nowrap">{this.props.user.user_name}</td>
+		    <td className="nowrap">{this.props.user.user_displayname}</td>
+		    <td className="nowrap count"><a href={'posts.php?user_id='+this.props.user.user_id}>{this.props.user.entries}</a></td>
+		</tr>
+	    );
+	);
+    }
+}
